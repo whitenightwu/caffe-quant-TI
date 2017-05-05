cfcebe85447c7e10322afe7b72a09b54a0da6a17
Manu Mathew <mathmanu@users.noreply.github.com>
2017-05-05 17:28:03 +0530

    Using prefetch flag, restructure

--- examples/tidsp/jacintopy/layers/ImageDataset.py
+++ examples/tidsp/jacintopy/layers/ImageDataset.py
@@ -5,23 +5,41 @@ import caffe
 import yaml
 from multiprocessing import (Process, Pipe)
 import atexit
-from LMDBReader import LMDBReader
+from ImageBatchReader import ImageBatchReader
 
 class ImageDataset(caffe.Layer):
   def setup(self, bottom, top):
     self._param_str = self.param_str
-    self._conn, conn = Pipe()
-    self._prefetcher = ImagePrefetcher(conn, self._param_str)
-    self._prefetcher.start()
-    def cleanup():
-      self._prefetcher.terminate()
-      self._prefetcher.join() 
-      self._conn.close()     
-    atexit.register(cleanup)
+    self._layer_params = yaml.load(self._param_str)    
+    self._prefetch = self._layer_params.get('prefetch')
+        
+    self._prefetcher = None
+    self._batch_reader = None  
+    if self._prefetch == True:
+      print('Using prefetch')
+      self._conn, conn = Pipe()    
+      self._prefetcher = ImagePrefetcher(conn, self._layer_params)
+      self._prefetcher.start()
+      def cleanup():
+        self._prefetcher.terminate()
+        self._prefetcher.join() 
+        self._conn.close()     
+      atexit.register(cleanup)
+    else:   
+      print('Not using prefetch')    
+      self._batch_reader = ImageBatchReader(self._layer_params)
+            
     self.reshape(bottom, top)
-    
+  
+  def next(self):
+    if self._prefetch == True:
+      blob = self._conn.recv()
+    else:
+      blob = self._batch_reader.next()  
+    return blob
+  
   def forward(self, bottom, top):
-    blob = self._conn.recv()
+    blob = self.next()      
     for i in range(len(blob)):
       top[i].reshape(*(blob[i].shape))
       top[i].data[...] = blob[i].astype(np.float32, copy=False)
@@ -31,62 +49,24 @@ class ImageDataset(caffe.Layer):
     pass
   
   def reshape(self, bottom, top):
-    blob = self._conn.recv()
+    blob = self.next()   
     for i in range(len(blob)):
-      top[i].reshape(*(blob[i].shape))
+      top[i].reshape(*(blob[i].shape))      
     return
       
-
     
 class ImagePrefetcher(Process):
-  def __init__(self, conn, param_str):
+  def __init__(self, conn, layer_params):
     super(ImagePrefetcher, self).__init__()
-    self._conn = conn
-    self._param_str = param_str
-    self._layer_params = yaml.load(self._param_str)
-    self._source = self._layer_params.get('source')
-    self._source_type = self._layer_params.get('source_type')
-    self._batch_size = self._layer_params.get('batch_size')
-    self._prefetch = self._layer_params.get('prefetch')
-    self._fetch_type = self._layer_params.get('fetch_type')
-    self._resize = self._layer_params.get('resize'); exec('self._resize='+self._resize);               
-    self._crop = self._layer_params.get('crop'); exec('self._crop='+self._crop);   
-    self._compressed = self._layer_params.get('compressed')          
-    self._reader = LMDBReader(self._source)
+    self._layer_params = layer_params  
+    self._conn = conn  
+    self._batch_reader = ImageBatchReader(self._layer_params)  
     return 
       
   def type(self):
     return 'ImagePrefetcher'
     
-  def decode_image_str(self,image_data):
-    if self._compressed:
-      buffer = StringIO(image_data)
-      #buffer.seek(0)
-      image_data = Image.open(buffer).convert('RGB')
-    if self._resize:
-      image_data = image_data.resize(self._resize, Image.ANTIALIAS)
-    if self._crop:
-      image_data = image_data.crop((0, 0, self._crop[0], self._crop[1]))
-    image_data = np.array(image_data).astype(np.float32)
-    image_bgr = image_data[..., ::-1]              #RGB->BGR
-    input_blob = image_bgr.transpose((2, 0, 1))    #Interleaved to planar
-    input_blob = input_blob[np.newaxis, ...]       #Introduce the batch dimension
-    return input_blob
-        
-  def next(self):  
-    image_batch = []
-    label_batch = []    
-    for i in range(self._batch_size):
-      data, label = self._reader.next()
-      image = self.decode_image_str(data)        
-      image_batch.extend(image)
-      label_batch.extend([label])
-    image_batch = np.array(image_batch)
-    label_batch = np.array(label_batch).reshape(self._batch_size, 1, 1, 1)
-    batch = [image_batch, label_batch]
-    return batch
-    
   def run(self):
     while True:
-      batch = self.next()
+      batch = self._batch_reader.next()
       self._conn.send(batch)
