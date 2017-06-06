1486534df9f692e40eef1dcfa1eb5927b39e1074
Manu Mathew <mathmanu@users.noreply.github.com>
2017-06-06 15:02:29 +0530

    [IMAGE_LABEL_DATA] Reverting to simpler implementation - but with fixed rand seed

--- include/caffe/layers/image_label_data_layer.hpp
+++ include/caffe/layers/image_label_data_layer.hpp
@@ -10,12 +10,13 @@
 #include "caffe/internal_thread.hpp"
 #include "caffe/layer.hpp"
 #include "caffe/layers/data_layer.hpp"
+#include "caffe/util/rng.hpp"
 #include "caffe/proto/caffe.pb.h"
 
 namespace caffe {
 
 template <typename Ftype, typename Btype>
-class ImageLabelDataLayer : public BasePrefetchingDataLayer<Ftype, Btype> {
+class ImageLabelDataLayer : public Layer<Ftype, Btype> {
  public:
   explicit ImageLabelDataLayer(const LayerParameter& param);
   virtual ~ImageLabelDataLayer();
@@ -24,23 +25,43 @@ class ImageLabelDataLayer : public BasePrefetchingDataLayer<Ftype, Btype> {
   const char* type() const override { return "ImageLabelData"; }
   int ExactNumBottomBlobs() const override { return 0; }
   int ExactNumTopBlobs() const override { return 2; }
-  void start_reading() override {}
-  bool ShareInParallel() const override {
-    return false;
+  
+  void Reshape(const vector<Blob*>& bottom, const vector<Blob*>& top) override;
+    
+  void Forward_cpu(const vector<Blob*>& bottom, const vector<Blob*>& top) override;
+	  
+  void Backward_cpu(const vector<Blob*>& top, const vector<bool>& propagate_down,
+      const vector<Blob*>& bottom) override {}
+  void Backward_gpu(const vector<Blob*>& top, const vector<bool>& propagate_down,
+      const vector<Blob*>& bottom) override {}
+	  	  
+  void InitRand(int seed) {
+    const unsigned int rng_seed = seed? seed : caffe_rng_rand();
+    rng_.reset(new Caffe::RNG(rng_seed));
   }
-  	
-  Flag* layer_inititialized_flag() override {
-    return this->phase_ == TRAIN ? &layer_inititialized_flag_ : nullptr;
+
+  unsigned int Rand() const {
+    CHECK(rng_);
+    caffe::rng_t *rng =
+        static_cast<caffe::rng_t *>(rng_->generator());
+    // this doesn't actually produce a uniform distribution
+    return static_cast<unsigned int>((*rng)());
   }
-  void InitializePrefetch() override;
-  void InternalThreadEntryN(size_t thread_id);
 
- protected:
-  void load_batch(Batch<Ftype>* batch, int thread_id, size_t queue_id = 0UL) override;
-  //bool is_gpu_transform() const { return true; }
+  unsigned int Rand(int n) const {
+    CHECK_GT(n, 0);
+    return Rand() % n;
+  }
 
+  //bool ShareInParallel() const override {
+  //  return false;
+  //}
+
+protected:
   shared_ptr<DataLayer<Ftype,Btype>> data_layer_, label_layer_;
-  Flag layer_inititialized_flag_;
+  shared_ptr<DataTransformer<Ftype>>  data_transformer_;
+  bool needs_rand_;
+  shared_ptr<Caffe::RNG> rng_;
 };
 
 
